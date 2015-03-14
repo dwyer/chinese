5518b8423268701c3e85f1571320545f6020739c
Casey Dwyer <caseydwyer@gmail.com>
2015-03-14 00:31:40 -0700

    Add TOCFL words to database.
    Signed-off-by: Casey Dwyer <caseydwyer@gmail.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+CURL=curl -O
+
 CEDICT_TXT2DB=cedict_txt2db.py
 
 CEDICT_DB=cedict.db
@@ -5,14 +7,20 @@ CEDICT_TXT=cedict_1_0_ts_utf-8_mdbg.txt
 CEDICT_TXT_GZ=$(CEDICT_TXT).gz
 CEDICT_TXT_GZ_URL=http://www.mdbg.net/chindict/export/cedict/$(CEDICT_TXT_GZ)
 
+TOCFL_CSV=TOCFL-tone-sort.csv
+TOCFL_CSV_URL=http://www.hackingchinese.com/media/$(TOCFL_CSV)
+
 all: $(CEDICT_DB)
 
-$(CEDICT_DB): $(CEDICT_TXT) $(CEDICT_TXT2DB)
-	./$(CEDICT_TXT2DB) $(CEDICT_TXT) $(CEDICT_DB)
+$(CEDICT_DB): $(CEDICT_TXT2DB) $(CEDICT_TXT) $(TOCFL_CSV)
+	./$(CEDICT_TXT2DB) $(CEDICT_TXT) $(TOCFL_CSV) $(CEDICT_DB)
 
 $(CEDICT_TXT):
-	curl -O $(CEDICT_TXT_GZ_URL)
+	$(CURL) $(CEDICT_TXT_GZ_URL)
 	gunzip $(CEDICT_TXT_GZ)
 
+$(TOCFL_CSV):
+	$(CURL) $(TOCFL_CSV_URL)
+
 clean:
-	$(RM) $(CEDICT_DB) $(CEDICT_TXT) $(CEDICT_TXT_GZ)
+	$(RM) $(CEDICT_DB) $(CEDICT_TXT) $(CEDICT_TXT_GZ) $(TOCFL_CSV)
--- cedict_txt2db.py
+++ cedict_txt2db.py
@@ -2,30 +2,44 @@
 
 # -*- encoding: utf-8 -*-
 
+import csv
 import os
 import re
 import sqlite3
 import sys
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print >>sys.stderr, 'usage: %s INPUT_FILE OUTPUT_FILE' % sys.argv[0]
     exit(1)
 
-text_file = sys.argv[1]
-data_file = sys.argv[2]
+cedict_file = sys.argv[1]
+tocfl_file = sys.argv[2]
+data_file = sys.argv[3]
 
 if os.path.exists(data_file):
     os.remove(data_file)
 
 connection = sqlite3.connect(data_file)
 cursor = connection.cursor()
-cursor.execute('CREATE TABLE words (traditional, simplified, pinyin, english)')
+cursor.execute('CREATE TABLE cedict (traditional, simplified, pinyin, english)')
+cursor.execute('CREATE TABLE tocfl (level, traditional, pinyin)')
+
+def decode_all(lst):
+    return [x.decode('utf-8') if isinstance(x, basestring) else x for x in lst]
 
 regex = re.compile(r'^([^\s]*) ([^\s]*) \[(.*)\] /(.*)/$')
-with open(text_file) as f:
+with open(cedict_file) as f:
     for line in f:
         if line.startswith('#'):
             continue
-        vals = [x.decode('utf-8') for x in regex.match(line.strip()).groups()]
-        cursor.execute('INSERT INTO words VALUES (?, ?, ?, ?)', vals)
+        cursor.execute('INSERT INTO cedict VALUES (?, ?, ?, ?)',
+                       decode_all(regex.match(line.strip()).groups()))
+
+with open(tocfl_file) as f:
+    reader = csv.reader(f)
+    reader.next()
+    for level, traditional, pinyin in reader:
+        cursor.execute('INSERT INTO tocfl VALUES (?, ?, ?)',
+                       decode_all([int(level), traditional, pinyin]))
+
 connection.commit()
